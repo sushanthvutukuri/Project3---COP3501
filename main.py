11fce2f4c39c4bb9aac323b6bb96b1f721a299e9
ismaelmaura <ismaelametller@gmail.com>
2023-12-03 23:27:03 -0500

    abc

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+import airlines
 
+
+#
+# ["Statistics"]["Flights"]["Total"]
+# get ratio of delayed flights vs on time in a monthly basis
+def processData(airports: list):
+    totalDelays = []  # Initialize an empty list
+
+    # ["Statistics"]["Flights"]["# of Delays"]
+    # calculate total number of delays which includes: Carrier, Late aircraft,
+    # National aviation system, Security, Weather.
+    for entry in airports:
+        totalDelay = (
+                entry["Statistics"]["# of Delays"]["Carrier"] +
+                entry["Statistics"]["# of Delays"]["Late Aircraft"] +
+                entry["Statistics"]["# of Delays"]["National Aviation System"] +
+                entry["Statistics"]["# of Delays"]["Security"] +
+                entry["Statistics"]["# of Delays"]["Weather"]
+        )
+        totalDelays.append(totalDelay)
+
+    max_value, max_index = max((value, index) for index, value in enumerate(totalDelays))
+    print("The airport with most delays is: " + airports[max_index]["Airport"]["Code"])
+    print(f"With total number of delays: {max_value}")
 
 if __name__ == '__main__':
-    print("hello")
+    airports = airlines.get_airports()
+    processData(airports)
