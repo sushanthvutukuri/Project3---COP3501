3d011d82c899abd6d987c86235060fdc85261ea1
ismaelmaura <ismaelametller@gmail.com>
2023-12-04 14:12:36 -0500

    performance analysis

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import airlines
 
 def insertion_sort(arr):
     """
-    Implementation of the Insertion Sort algorithm.
+    Implementation of the Insertion Sort algorithm for tuples.
     """
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
+        while j >= 0 and key[0] < arr[j][0]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
 
 def selection_sort(arr):
     """
-    Implementation of the Selection Sort algorithm.
+    Implementation of the Selection Sort algorithm for tuples.
     """
     n = len(arr)
 
     for i in range(n):
         min_index = i
         for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
+            if arr[j][0] < arr[min_index][0]:
                 min_index = j
 
         # Swap the found minimum element with the first element
@@ -30,18 +30,14 @@ def selection_sort(arr):
 
 def processData(airports: list):
     totalDelays = []  # Initialize an empty list
-    totalOnTime = []
-    delayRatio = []
 
     # ["Statistics"]["Flights"]["# of Delays"]
     # calculate total number of delays which includes: Carrier, Late aircraft,
     # National aviation system, Security, Weather.
     for entry in airports:
-        temp1 = entry["Statistics"]["Flights"]["Delayed"]
-        temp2 = entry["Statistics"]["Flights"]["On Time"]
-        totalDelays.append(temp1)
-        totalOnTime.append(temp2)
-        delayRatio.append(temp1 / temp2)
+        temp1DelayTime = entry["Statistics"]["Flights"]["Delayed"]
+        temp2AirportCode = entry["Airport"]["Code"]
+        totalDelays.append((temp1DelayTime, temp2AirportCode))
 
     # Use Selection Sort to sort the airports based on the total number of delays
     start_time = time.time()
@@ -66,14 +62,10 @@ def processData(airports: list):
     end_time = time.time()
     print(f"Built-in Sort Time: {end_time - start_time} seconds")
 
+    # print the list of delays from least to most delays
+    for entries in totalDelays:
+        print(entries)
 
 if __name__ == '__main__':
     airports = airlines.get_airports()
     processData(airports)
-
-
-# in my computer the results of the performance analysis were:
-# Performance Analysis:
-# Selection Sort Time: 0.46306920051574707 seconds
-# Insertion Sort Time: 0.0005593299865722656 seconds
-# Built-in Sort Time: 3.814697265625e-05 seconds
