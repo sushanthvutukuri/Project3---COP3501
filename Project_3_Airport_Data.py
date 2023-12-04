ce138bbb358a18f401f200cc85dd63dbca164026
sushanthvutukuri <133739204+sushanthvutukuri@users.noreply.github.com>
2023-12-04 16:29:37 -0500

    Add files via upload

--- Project_3_Airport_Data.py
+++ Project_3_Airport_Data.py
@@ -15,7 +15,7 @@ class Data:
         for index in airports:
             if self.airlist.count(index["Airport"]) == 0:
                 self.airlist.append(index["Airport"])
-    #Method that returns data in grpah format
+    #Method that returns data in graph format
     @staticmethod
     def printData(code, self):
         airdata = []
@@ -23,27 +23,34 @@ class Data:
         counter=0
         airport_name="";
         print("Following is a graph and text representation of the data requested.")
+        #loop for finding the specific airport or code to see if it exists
         for index in self.airlist:
             if index["Code"] == code or index["Name"] == code:
                 print(index["Name"])
                 airport_name=index["Name"]
                 break
+
+        #if the specific airport does exist, add all the statistics related to that airport into sets
         for index in self.airports:
             if(index["Airport"]["Code"]==code):
                 print(index["Time"])
                 print(index["Statistics"]["Flights"])
                 airdata.append(index["Statistics"]["Flights"]["Total"]-index["Statistics"]["Flights"]["On Time"])
                 airtime.append(index["Time"]["Label"])
+        #if the length of the sets containng the data is 0, then the airport does not exist so return and display that it was an invalid code
         if(len(airdata)==0):
             print("Invalid code or name. Please make sure that your spelling is correct. Codes are case sensitive and must be capitalized. Names must be specific.")
             print()
             return
+
+        #create a plot of the data at one ariport over a specific period of time
         fig, ax=plt.subplots(figsize=(20, 4), layout='constrained')
         string="Date (From "+(airtime[0])+" to "+(airtime[-1])+")"
         ax.plot(airtime, airdata)
         ax.set_xlabel(string)
         ax.set_ylabel("Number of Delays")
         ax.set_title("Trend for Delays at " + airport_name)
+        #display information about the plot
         print()
         print("NOTE: Unfortunately, due to large data sets, the X-Axis is may be unreadable. To counteract this issue, please use the zoom in feature on the graph ")
         print()
@@ -161,33 +168,38 @@ def processData(airports: list):
         #print(entries)
 
 if __name__ == '__main__':
+    #main UI using input output system
     data= Data()
     print("This is a Flight Data Compiler that retrieves and stores data using a sorting algorithm based on delayed and missed flights.")
     print("These are the following commands:")
     cont=True
     while cont:
+        #displays the commands available
         print("Press 1 to generate a graph and data based on an airport code or name")
-        print("Press 2 to generate a list of all airports sorted in ascending order based on the number of flights delayed")
+        print("Press 2 to generate a graph of all airports flight delays averaged over multiple months")
         print("Press 3 to exit")
         print()
         print("Type in command and hit enter:")
         num=input()
         print()
         if num=="1":
+            #displays statistics of flight delays at one airport
             print("Enter the airport code or name: ")
             code = input()
             data.printData(code, data)
-            #data.getData(code, data)
         elif num=="2":
+            #displays statistics of flight delays at all airports in the dataset averaged over multiple months
             print("The graph displays the average delays at an airport by taking into account the delays over multiple months")
             print()
             print("Once you are finished with the graph, please close to continue using the Compiler")
             airports = airlines.get_airports()
             processData(airports) 
         elif num=="3":
+            #command for exit program
             print("Thank you for using this program!")
             cont=False
         else:
+            #if command is invalid displays this error
             print("Invalid command, please try again")
             print()
 
